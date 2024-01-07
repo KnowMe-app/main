88535322573e759d73c3a0b9c0bdb0259aa4895e
xanderkiev2022 <301019a@gmail.com>
2024-01-07 13:51:55 +0200

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 700px;
+  max-width: 1280px;
   min-height: calc(100vh - 400px);
   margin: 30px auto 30px;
   padding: 30px;
@@ -9,6 +9,7 @@ export const Container = styled.div`
   box-shadow: var(--box-shadow);
   display: flex;
   flex-direction: column;
+  background: linear-gradient(to bottom, #ffa500, #ff8c00);
 `;
 
 export const TitleH1 = styled.h1`
