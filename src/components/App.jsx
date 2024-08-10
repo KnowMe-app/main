86147442b754ae0b442f059989f34db5d6e31e76
Alex Koval <113243861+xanderkiev2022@users.noreply.github.com>
2024-08-10 20:51:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes, useNavigate  } from 'react-router-dom';
 import { PrivacyPolicy } from './PrivacyPolicy';
-// import {ProfileScreen} from './ProfileScreen';
+import {ProfileScreen} from './ProfileScreen';
 import { LoginScreen } from './LoginScreen';
 import { SubmitForm } from './SubmitForm';
 
@@ -22,7 +22,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<PrivacyPolicy />} />
       <Route path="/submit" element={<SubmitForm />} />
-      {/* <Route path="/profile"  element={<ProfileScreen isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} /> */}
+      <Route path="/profile"  element={<ProfileScreen isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
       <Route path="/login" element={<LoginScreen isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
     </Routes>
   );
