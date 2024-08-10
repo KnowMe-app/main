b6009cf07c15fda6d5368feeeed1056ce12d1cb6
Alex Koval <113243861+xanderkiev2022@users.noreply.github.com>
2024-08-10 19:03:56 +0300

    rollback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, Navigate, useNavigate  } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { PrivacyPolicy } from './PrivacyPolicy';
-import {ProfileScreen} from './ProfileScreen';
-import { LoginScreen } from './LoginScreen';
-
-// import React, {  } from 'react';
-// import { Route, Routes, Navigate,  } from 'react-router-dom';
-// import { PrivacyPolicy } from './PrivacyPolicy';
-
+import { SubmitForm } from './SubmitForm';
 
 export const App = () => {
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log('isLoggedIn :>> ', isLoggedIn);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate('/submit');
-    }
-  }, [isLoggedIn, navigate]);
-
-
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/privacy_policy" />} />
-      <Route path="/privacy_policy" element={<PrivacyPolicy />} />
-      <Route path="/submit"  element={<ProfileScreen isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
-      <Route path="/login" element={<LoginScreen isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+      <Route path="/" element={<PrivacyPolicy />} />
+      <Route path="/submit" element={<SubmitForm />} />
     </Routes>
   );
 };
