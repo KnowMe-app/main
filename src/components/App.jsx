40116fbc10d36da0ff323120e92f12c9a6cef930
Alex Koval <113243861+xanderkiev2022@users.noreply.github.com>
2024-11-08 21:16:32 +0200

    special page for default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,13 @@ import {ProfileScreen} from './ProfileScreen';
 import { LoginScreen } from './LoginScreen';
 import { SubmitForm } from './SubmitForm';
 import {AddNewProfile} from './AddNewProfile';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './config';
 
 export const App = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(false);
   console.log('isLoggedIn :>> ', isLoggedIn);
 
   const navigate = useNavigate();
@@ -19,13 +22,25 @@ export const App = () => {
     }
   }, [isLoggedIn, navigate]);
 
+  // Special page for admin
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user && user.uid === process.env.REACT_APP_USER1) {
+       setUser(true)
+      } 
+    });
+    // Clean up the subscription on component unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<PrivacyPolicy />} />
+      <Route path="/" element={user ? <AddNewProfile isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> : <PrivacyPolicy />} />
       <Route path="/submit" element={<SubmitForm />} />
       <Route path="/profile"  element={<ProfileScreen isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
       <Route path="/login" element={<LoginScreen isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
-      <Route path="/add" element={<AddNewProfile isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+      {user&& <Route path="/add" element={<AddNewProfile isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />}
+      {user&&<Route path="/policy" element={<PrivacyPolicy/>} />}
     </Routes>
   );
 };
