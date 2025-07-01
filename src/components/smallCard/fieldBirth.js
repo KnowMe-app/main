07d46c0b186d31015d31434c2033824c1a084e6b
Alex Koval <113243861+KnowMe-app@users.noreply.github.com>
2025-07-01 08:51:10 +0300

    Add colored display for age and BMI

--- src/components/smallCard/fieldBirth.js
+++ src/components/smallCard/fieldBirth.js
@@ -1,7 +1,10 @@
 import { utilCalculateAge } from "./utilCalculateAge";
+import { AttentionDiv } from 'components/styles';
 
 export const fieldBirth = birth => {
   const age = utilCalculateAge(birth);
 
-  return age !== null ? <span>{age}р</span> : null;
+  return age !== null ? (
+    <AttentionDiv>{age}р</AttentionDiv>
+  ) : null;
 };
