a5eb5c04596c5bee871a9aa5815669a199e0e43b
Alex Koval <113243861+KnowMe-app@users.noreply.github.com>
2025-07-19 21:44:08 +0300

    fix: center cursor in search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -46,7 +46,7 @@ const InputField = styled.textarea`
   border: none;
   outline: none;
   flex: 1;
-  padding-left: 10px;
+  padding: 4px 0 4px 10px;
   max-width: 100%;
   min-width: 0;
   pointer-events: auto;
