79d1d7f88866e13eeaab5addec0bac8848a55184
Alex Koval <113243861+KnowMe-app@users.noreply.github.com>
2025-07-31 14:14:10 +0300

    Change donor name order to name first on matching cards

--- src/components/Matching.jsx
+++ src/components/Matching.jsx
@@ -653,7 +653,7 @@ const SwipeableCard = ({
             <Info>
               <Title>Egg donor</Title>
               <DonorName>
-                {(getCurrentValue(user.surname) || '').trim()} {(getCurrentValue(user.name) || '').trim()}{user.birth ? `, ${utilCalculateAge(user.birth)}` : ''}
+                {(getCurrentValue(user.name) || '').trim()} {(getCurrentValue(user.surname) || '').trim()}{user.birth ? `, ${utilCalculateAge(user.birth)}` : ''}
               </DonorName>
               <br />
               {[
@@ -1260,7 +1260,7 @@ const Matching = () => {
               <Info>
                 <Title>Egg donor</Title>
                 <DonorName>
-                  {(getCurrentValue(selected.surname) || '').trim()} {(getCurrentValue(selected.name) || '').trim()}{selected.birth ? `, ${utilCalculateAge(selected.birth)}р` : ''}
+                  {(getCurrentValue(selected.name) || '').trim()} {(getCurrentValue(selected.surname) || '').trim()}{selected.birth ? `, ${utilCalculateAge(selected.birth)}р` : ''}
                 </DonorName>
                 <br />
                 {[
